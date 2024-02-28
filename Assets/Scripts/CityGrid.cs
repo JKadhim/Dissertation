095adcf3614768c65ea33b00fa5eedff1623983f
C0053742 <c0053742@newcastle.ac.uk>
2024-02-28 15:49:17 +0000

    added generation around the player, need to add load distance next

--- Assets/Scripts/CityGrid.cs
+++ Assets/Scripts/CityGrid.cs
@@ -6,37 +6,132 @@ using UnityEngine;
 public class CityGrid : MonoBehaviour
 {
 
-    public int citySize;
+    public int citySize = 5;
     public CityBlock cityBlock;
     public GameObject player;
+    
+    private Vector3 startPosition;
+    private Hashtable blockContainer = new Hashtable();
+    private GameObject cityContainer;
+
+    private int xPlayerMove
+    {
+        get
+        {
+            return (int)(player.transform.position.x - startPosition.x);
+        }
+    }
+    private int zPlayerMove
+    {
+        get
+        {
+            return (int)(player.transform.position.z - startPosition.z);
+        }
+    }
+    private int playerLocationX
+    {
+        get
+        {
+            if (player.transform.position.x < 0)
+            {
+                return (int)Mathf.Floor(player.transform.position.x / (citySize * 2 * cityBlock.blockSize * cityBlock.cellSize)) * (citySize * 2 * cityBlock.blockSize * cityBlock.cellSize);
+            }
+            else
+            {
+                return (int)Mathf.Ceil(player.transform.position.x / (citySize * 2 * cityBlock.blockSize * cityBlock.cellSize)) * (citySize * 2 * cityBlock.blockSize * cityBlock.cellSize);
+            }
+        }
+    }
+
+    private int playerLocationZ
+    {
+        get
+        {
+            if (player.transform.position.z < 0)
+            {
+                return (int)Mathf.Floor(player.transform.position.z / (citySize * 2 * cityBlock.blockSize * cityBlock.cellSize)) * (citySize * 2 * cityBlock.blockSize * cityBlock.cellSize);
+            }
+            else
+            {
+                return (int)Mathf.Ceil(player.transform.position.z / (citySize * 2 * cityBlock.blockSize * cityBlock.cellSize)) * (citySize * 2 * cityBlock.blockSize * cityBlock.cellSize);
+            }
+        }
+    }
 
     private void Start()
     {
+        cityContainer = new GameObject("CityContainer");
         CitySpawn();
     }
 
     //Spaces out individual city blocks into a grid formation
+    
+    private void Update()
+    {
+        int blockSize = cityBlock.blockSize;
+        int cellSize = cityBlock.cellSize;
+        int scale = blockSize * citySize * cellSize;
+        int cityOffset = citySize * 2;
+        
+        if (HasPlayerMoved())
+        {
+            for (int x = -citySize; x < citySize; x++)
+            {
+                for (int z = -citySize; z < citySize; z++)
+                {
+
+                    Vector3 pos = new Vector3((x * blockSize * cellSize)  + playerLocationX, 0, (z * blockSize * cellSize) + playerLocationZ);
+
+                    if (!blockContainer.ContainsKey(pos))
+                    {
+                        GameObject block = cityBlock.BlockSpawn(x, z);
+
+                        block.transform.position = pos;
+                        block.transform.SetParent(cityContainer.transform);
+
+                        blockContainer.Add(pos, block);
+                    }
+
+                }
+            }
+        }
+    }
+
     private void CitySpawn()
     {
-        GameObject cityContainer = new GameObject("CityContainer");
-        float offset = citySize / 2;
-        int blockSize = cityBlock.blockSize;
 
-        for (int y = 0; y < citySize; y++)
+        int blockSize = cityBlock.blockSize;
+        int cellSize = cityBlock.cellSize;
+        int scale = blockSize * citySize * cellSize;
+
+        for (int x = -citySize; x < citySize; x++)
         {
-            for (int x = 0; x < citySize; x++)
+            for (int z = -citySize; z < citySize; z++)
             {
-                GameObject block =  cityBlock.BlockSpawn(x,y);
-                
+
                 //sets the blocks an appropriate distance apart
-                block.transform.position = new Vector3((x * blockSize * cityBlock.cellSize), 0, y * blockSize * cityBlock.cellSize);
+                Vector3 pos = new Vector3((x * blockSize * cellSize) + startPosition.x, 0, (z * blockSize * cellSize) + startPosition.z);
+
+                GameObject block = cityBlock.BlockSpawn(x, z);
+
+                block.transform.position = pos;
                 block.transform.SetParent(cityContainer.transform);
+
+                blockContainer.Add(pos, block);
 
             }
         }
-        int cellSize = cityBlock.cellSize;
-        int scale = blockSize * citySize * cellSize;
-        Vector3 containerPosition = cityContainer.transform.position;
-        player.transform.position = new Vector3 ((containerPosition.x + cellSize + scale) / 2, 1, (containerPosition.z + cellSize + scale) /2);
+    }
+
+    bool HasPlayerMoved()
+    {
+        if (Mathf.Abs(xPlayerMove) >= citySize|| Mathf.Abs(zPlayerMove) >= citySize)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
