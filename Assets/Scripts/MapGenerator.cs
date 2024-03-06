45943ae10e7ec724e47d10c4141d7c476ede628b
C0053742 <c0053742@newcastle.ac.uk>
2024-03-06 19:19:49 +0000

    created a way to visualise districts/zones on a plane. need to alter the noise generation

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public enum DrawMode {NoiseMap, ColourMap};
+    
+    public DrawMode drawMode;
+    
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public int octaves;
+    [Range(0f, 1f)]
+    public float persistence;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
+    public bool autoUpdate;
+
+    public TerrainType[] zones;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, seed, noiseScale, octaves, persistence, lacunarity, offset);
+
+        Color[] colourMap = new Color[mapWidth * mapHeight];
+
+        for (int y = 0;  y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < zones.Length;  i++)
+                {
+                    if (currentHeight <= zones[i].height)
+                    {
+                        colourMap[y * mapWidth + x] = zones[i].colour;
+                        break;
+                    }
+                }
+            }
+        }
+
+        MapDisplay display = FindFirstObjectByType<MapDisplay>();
+        
+        if (drawMode == DrawMode.NoiseMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        }
+        else if (drawMode == DrawMode.ColourMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if (mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+        if (lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
+    }
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color colour;
+}
---
+++ Assets/Scripts/Visualization.cs
@@ -0,0 +1,12 @@
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+using static Unity.Mathematics.math;
+
+public class NoiseVisualization : MonoBehaviour
+{
+    
+}
